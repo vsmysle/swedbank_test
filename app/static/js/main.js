a5ec402e943b6facfe5b49be5085916aa1af7c6f
vsmysle <kyrylo.voronchenko@gmail.com>
2017-03-04 20:06:30 +0200

    add map

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,36 +1,22 @@
 /**
  * Created by vsmysle on 3/4/17.
  */
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-
-var geojson = new L.geoJson();
-geojson.addTo(map);
-
 $( document ).ready(function(){
-    $.getJSON($SCRIPT_ROOT+"/get_full_data", function (data) {
+    var myStyle = {
 
-    });
+
+    };
     var mapboxAccessToken = "pk.eyJ1IjoidnNteXNsZSIsImEiOiJjaXp2aHN1YnQwMDBnMnFwbms0c3JlZnJyIn0.by1qV2ysk6yiAFCc-ADnyQ";
-    var map = L.map('mapid', {
+    var map = L.map('map_container', {
         center: [51.505, -0.09],
         zoom: 13
     });
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
-            id: 'mapbox.light'
-        }).addTo(map);
 
-        var feature = data['geo_data'];
-        console.log(data['geo_data']);
-        L.GeoJSON.AJAX(data, {
-            style: function (feature) {
-                return {color: feature.properties.color};
-            }
-        }).bindPopup(function (layer) {
-            return layer.feature.properties.description;
-        }).addTo(map);
-    })
-});
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+        id: 'mapbox.light'
+    }).addTo(map);
+    $.getJSON($SCRIPT_ROOT+"/get_full_data", function (data) {
+        var geojson = data['geo_data'];
+        geojson.addTo(map);
+    });
+});s
