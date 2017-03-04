7d8249e13134e6f6585ca81e926f23f15b6ea07c
ddnomad <ddnomad@protonmail.com>
2017-03-04 21:02:30 +0200

    test

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -8,15 +8,15 @@ $( document ).ready(function(){
     };
     var mapboxAccessToken = "pk.eyJ1IjoidnNteXNsZSIsImEiOiJjaXp2aHN1YnQwMDBnMnFwbms0c3JlZnJyIn0.by1qV2ysk6yiAFCc-ADnyQ";
     var map = L.map('map_container', {
-        center: [51.505, -0.09],
-        zoom: 13
+        center: [58.598014, 25.015606],
+        zoom: 8
     });
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
         id: 'mapbox.light'
     }).addTo(map);
-    $.getJSON($SCRIPT_ROOT+"/get_full_data", function (data) {
-        var geojson = data['geo_data'];
-        geojson.addTo(map);
-    });
+    // $.getJSON($SCRIPT_ROOT+"/get_full_data", function (data) {
+    //     var geojson = data['geo_data'];
+    //     geojson.addTo(map);
+    // });
 });s
